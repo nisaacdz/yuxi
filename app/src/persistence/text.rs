db7b27c633e4e0a766a15723834e0bfd4fca4fb1
Isaac Dzikum <nisaacdz@gmail.com>
2025-03-28 03:38:12 +0000

    Unlabeled implementations

--- app/src/persistence/text.rs
+++ app/src/persistence/text.rs
@@ -1,24 +1,41 @@
-use sea_orm::{DbConn, DbErr, EntityTrait};
+use fake::Fake;
+use sea_orm::{ActiveModelTrait, ActiveValue::Set, DbConn, DbErr, EntityTrait};
 
 use models::domains::{texts, tournaments};
 
 pub async fn get_or_generate_text(db: &DbConn, tournament_id: String) -> Result<String, DbErr> {
     let tournament = tournaments::Entity::find_by_id(tournament_id.clone())
         .one(db)
-        .await
-        .unwrap();
+        .await?;
 
-    let text_id = tournament.map(|t| t.text_id).flatten();
+    let text_id = tournament.as_ref().map(|t| t.text_id).flatten();
 
-    if let Some(text_id) = text_id {
-        let text = texts::Entity::find_by_id(text_id).one(db).await.unwrap();
-
-        if let Some(text) = text {
-            return Ok(text.content);
-        }
+    let text_from_id = if let Some(text_id) = text_id {
+        texts::Entity::find_by_id(text_id)
+            .one(db)
+            .await?
+            .map(|t| t.content)
     } else {
-        return Ok("In the land of myth and in the time of magic, the destiny of a great kingdom rests on the shoulders of a young boy. He's named Merlin.".to_string());
-    }
+        None
+    };
 
-    return Ok("In the land of myth and in the time of magic, the destiny of a great kingdom rests on the shoulders of a young boy. He's named Merlin.".to_string());
+    let text = if let Some(text_from_id) = text_from_id {
+        text_from_id
+    } else {
+        let text_options = tournament
+            .as_ref()
+            .map(|t| t.text_options.clone())
+            .flatten();
+        let new: String = fake::faker::lorem::en::Paragraph(3..5).fake();
+        models::domains::texts::ActiveModel {
+            content: Set(new.clone()),
+            options: Set(text_options),
+            ..Default::default()
+        }
+        .save(db)
+        .await?;
+        new
+    };
+
+    return Ok(text);
 }
