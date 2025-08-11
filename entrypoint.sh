24a03deb8715e54cf71bf45ff947e706255a39cd
Isaac <nisaacdz@gmail.com>
2025-08-11 00:29:52 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Print all environment variables to the container's log
-echo "--- Printing environment variables at execution time ---"
-printenv
-echo "----------------------------------------------------"
+# # Print all environment variables to the container's log
+# echo "--- Printing environment variables at execution time ---"
+# printenv
+# echo "----------------------------------------------------"
 
 # Now, execute the main application
 # The 'exec' command replaces the shell process with your app's process
